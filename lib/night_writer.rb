5b277dac16b8c40cb2258992150b8ef671126449
Logan Riffell <lkriffell@gmail.com>
2020-07-31 12:52:37 -0600

    Add attr_reader and initialize method for night writer class

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -4,5 +4,13 @@ require_relative "dictionary"
 
 class NightWriter
 
+  attr_reader :reader,
+              :writer,
+              :dictionary
+  def initialize
+    @reader = FileReader.new
+    @writer = FileWriter.new
+    @dictionary = Dictionary.new
+  end
 
 end
