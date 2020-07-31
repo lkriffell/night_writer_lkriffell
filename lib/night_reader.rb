08a6a0e82e2ee4ec856be479f5fcc860c6aed2ea
Logan Riffell <lkriffell@gmail.com>
2020-07-31 17:58:02 -0600

    Add print_sample_line method

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -10,7 +10,7 @@ class NightReader
 
   def initialize
     # For testing: remove @ from argv wherever used when ready to run from comman line
-    @ARGV = ["message.txt", "braille.txt"]
+    @ARGV = ["braille.txt", "original_message.txt"]
     @reader = FileReader.new
     @writer = FileWriter.new
     @dictionary = Dictionary.new
@@ -19,4 +19,14 @@ class NightReader
     # Uncomment when ready to run
     # print_sample_line
   end
+
+  def print_sample_line
+    char_count = @reader.read(@ARGV[0]).chomp.length
+    sample_line = "Created '#{@ARGV[1]}' containing #{char_count} characters"
+    puts sample_line
+    sample_line
+  end
 end
+# night_reader = NightReader.new
+# night_reader.write_file(night_reader.braille, ARGV[1])
+# require "pry"; binding.pry
