b7bbfa6dd1e031f8d94eca763bc704e6094f790d
Logan Riffell <lkriffell@gmail.com>
2020-07-31 12:52:24 -0600

    Add test_it_has_attributes for night_writer class

--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -10,4 +10,10 @@ class NightWriterTest < Minitest::Test
   def test_it_exists
     assert_instance_of NightWriter, @encoder
   end
+
+  def test_it_has_attributes
+    assert_instance_of FileReader, @encoder.reader
+    assert_instance_of FileWriter, @encoder.writer
+    assert_instance_of Dictionary, @encoder.dictionary
+  end
 end
