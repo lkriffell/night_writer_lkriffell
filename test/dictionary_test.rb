5755291b4e99949f9d6a33623c3345a73fefa846
Logan Riffell <lkriffell@gmail.com>
2020-07-31 20:06:18 -0600

    Update assert_equal to work with updated dictionary

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -12,15 +12,15 @@ class DictionaryTest < Minitest::Test
   def test_it_has_attributes
     dictionary = Dictionary.new
     dictionary.character_to_braille_key
-    # to_braille_conversion is split for testing purposes
-    assert_equal ["00", "..", "0."], dictionary.to_braille_conversion["m"].split("|")
+
+    assert_equal "00..0.", dictionary.to_braille_conversion["m"]
   end
 
   def test_to_english_conversion
     dictionary = Dictionary.new
     dictionary.character_to_braille_key
 
-    assert_equal "b", dictionary.to_english_conversion["0.|0.|..|"]
+    assert_equal "b", dictionary.to_english_conversion["0.0..."]
   end
 
 end
