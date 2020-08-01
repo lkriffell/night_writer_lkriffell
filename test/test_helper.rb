7dc086524a6d4888c11ae86478a0bc95aee48ce3
Logan Riffell <lkriffell@gmail.com>
2020-07-31 20:18:30 -0600

    Add test_helper which holds simplecov

---
+++ test/test_helper.rb
@@ -0,0 +1,9 @@
+require "simplecov"
+SimpleCov.start
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/dictionary"
+require "./lib/file_reader"
+require "./lib/file_writer"
+require "./lib/night_reader"
+require "./lib/night_writer"
