ad50feb4da020a79b876646f892a90379f89f4b5
Logan Riffell <lkriffell@gmail.com>
2020-08-02 17:40:44 -0600

    Update dictionary test to remove redundancy

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -1,25 +1,21 @@
 require "./test/test_helper"
-# require "./lib/dictionary"
-# require "minitest/autorun"
-# require "minitest/pride"
 
 class DictionaryTest < Minitest::Test
 
   def test_it_exists
     dictionary = Dictionary.new
+    
     assert_instance_of Dictionary, dictionary
   end
 
   def test_it_has_attributes
     dictionary = Dictionary.new
-    dictionary.character_to_braille_key
 
     assert_equal "00..0.", dictionary.to_braille_conversion["m"]
   end
 
   def test_to_english_conversion
     dictionary = Dictionary.new
-    dictionary.character_to_braille_key
 
     assert_equal "b", dictionary.to_english_conversion["0.0..."]
   end
