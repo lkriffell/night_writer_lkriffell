0e55becbc0365749851bc55fb517343d64fd8f6b
Logan Riffell <lkriffell@gmail.com>
2020-08-03 14:02:11 -0600

    Update to remove redundant code

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -3,14 +3,6 @@ require_relative "dictionary"
 require_relative "translator"
 include Fileable
 
-  if ARGV == [] || ARGV == ["message.txt", "braille.txt"]
-    2.times do
-      ARGV.pop
-    end
-    ARGV << "braille.txt"
-    ARGV << "original_message.txt"
-  end
-
   translator = Translator.new
   english = translator.encode_to_english
   write(english, ARGV[1])
--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -3,17 +3,9 @@ require_relative "dictionary"
 require_relative "translator"
 include Fileable
 
-  if ARGV == [] || ARGV == ["braille.txt", "original_message.txt"]
-    2.times do
-      ARGV.pop
-    end
-    ARGV << "message.txt"
-    ARGV << "braille.txt"
-  end
-
   translator = Translator.new
   braille = translator.arrange_for_output
   write(braille, ARGV[1])
   puts translator.create_sample_line
-  
+
 # ruby ./lib/night_writer.rb message.txt braille.txt
